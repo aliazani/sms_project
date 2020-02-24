7c9a2905e2507813e50489e2ea03f4be3385501b
aliazani <46054441+aliazani@users.noreply.github.com>
2020-02-25 00:12:30 +0330

    scale of what we want to do

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-print('Start')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+
+@app.route('/v1/get_sms')
+def get_sms():
+    pass
+
+
+def send_sms():
+    pass
+
+
+def check_serial():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
