d2f1a7425437ef370a8d173cd9fd0d87699b7b1b
Aliaz <aliazn2013@yahoo.com>
2020-02-28 18:00:39 +0330

    Write the excel reader.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from pandas import read_excel
+import requests
+import kave_negar
 
 app = Flask(__name__)
 
@@ -8,13 +11,32 @@ def hello():
     return 'Hello World'
 
 
-@app.route('/v1/get_sms')
-def get_sms():
-    pass
+def import_database_from_excel(file_path):
+    """
+    Get an excel file name and import lookup data (data and failure) from it.
+    """
+    data_frame = read_excel(file_path, 0)
+    for index, (row, reference_number, description, start_serial, end_serial, date) in data_frame.iterrows():
+        pass
+    data_frame = read_excel(file_path, 1)  # This contains fail serial numbers.
+    for index, failed_serial in data_frame.iterrows():
+        fail_serial_row = failed_serial[0]
 
 
-def send_sms():
-    pass
+def send_sms(receptor, message):
+    url = f'https://api.kavenegar.com/v1/{kave_negar.API_KEY}/sms/send.json'
+    data = {'receptor': receptor,
+            'message': message}
+    response = requests.post(url, data)
+
+
+@app.route('/v1/process', methods=['POST'])
+def process():
+    data = request.form
+    sender = data['from']
+    message = data['message']
+    send_sms(sender, 'Hi' + message)
+    return jsonify(data), 200
 
 
 def check_serial():
@@ -22,4 +44,4 @@ def check_serial():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000, debug=True)
